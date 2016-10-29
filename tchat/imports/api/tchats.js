73d947f1d4df2a9f91fbe8ff7a9357ca9b99aa00
Arkolips <catel.ludovic@gmail.com>
2016-10-29 10:49:09 +0200

    insert users and public/private status

--- tchat/imports/api/tchats.js
+++ tchat/imports/api/tchats.js
@@ -1,3 +1,81 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tchats = new Mongo.Collection('tchats');
+
+if (Meteor.isServer) {
+  // Ce code se lance uniquement coté serveur
+  Meteor.publish('tchats', function tchatsPublication() {
+    return Tchats.find({
+      $or: [{
+        private: {
+          $ne: true
+        }
+      }, {
+        owner: this.userId
+      }, ],
+    });
+  });
+}
+
+Meteor.methods({
+  'tchats.insert' (text) {
+    check(text, String);
+
+    // On verifie que l'utilisateur existe avant d'insérer le message
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('Vous n\'êtes pas autoriser à faire ça !');
+    }
+    Tchats.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+  },
+  'tchats.remove' (tchatId) {
+    check(tchatId, String);
+
+    const tchat = Tchats.findOne(tchatId);
+    if (tchat.private && tchat.owner !== Meteor.userId()) {
+      // On s'assure que seul le propriétaire du message peut le supprimer
+      throw new Meteor.Error('Vous n\'êtes pas autorisé à faire ça');
+    }
+
+    Tchats.remove(tchatId);
+  },
+  'tchats.setChecked' (tchatId, setChecked) {
+    check(tchatId, String);
+    check(setChecked, Boolean);
+
+    const tchat = Tchats.findOne(tchatId);
+    if (tchat.private && tchat.owner !== Meteor.userId()) {
+      // On s'assure que seul le propriétaire puisse la mettre sur off si privé
+      throw new Meteor.Error('Vous n\'êtes pas autorisé à faire ça');
+    }
+
+    Tchats.update(tchatId, {
+      $set: {
+        checked: setChecked
+      }
+    });
+  },
+  'tchats.setPrivate' (tchatId, setToPrivate) {
+    check(tchatId, String);
+    check(setToPrivate, Boolean);
+
+    const tchat = Tchats.findOne(tchatId);
+
+    // On s'assure que les propriétaire peuvent créer un message privé
+    if (tchat.owner !== Meteor.userId()) {
+      throw new Meteor.Error('Vous n\'êtes pas autorisé');
+    }
+
+    Tchats.update(tchatId, {
+      $set: {
+        private: setToPrivate
+      }
+    });
+  },
+});
--- tchat/imports/components/tchatHome/tchatHome.js
+++ tchat/imports/components/tchatHome/tchatHome.js
@@ -9,6 +9,8 @@ class TchatCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.subscribe('tchats');
+
     this.helpers({
       tchats() {
         return Tchats.find({}, {
@@ -25,15 +27,23 @@ class TchatCtrl {
 
   addMessage(newMessage) {
     // Insert un message dans la bd
-    Tchats.insert({
-      text: newMessage,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tchats.insert', newMessage);
 
     // Vider la zone de saisie
     this.newMessage = '';
+  }
+
+  setChecked(tchat) {
+    // On verifie que la propriété n'est pas son opposée
+    Meteor.call('tchats.setChecked', tchat._id, !tchat.checked);
+  }
+
+  removeMessage(tchat) {
+    Meteor.call('tchats.remove', tchat._id);
+  }
+
+  setPrivate(tchat) {
+    Meteor.call('tchats.setPrivate', tchat._id, !tchat.private);
   }
 }
 
