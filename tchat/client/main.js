af98209dd78bf3a2449924161d33d4b70783728e
Arkolips <catel.ludovic@gmail.com>
2016-10-29 09:20:33 +0200

    fix CSS and process

--- tchat/client/main.js
+++ tchat/client/main.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import tchatHome from '../imports/components/tchatHome/tchatHome';
+import '../imports/startup/accounts-config.js';
 
 angular.module('tchat', [
   angularMeteor,
-  tchatHome.name
+  tchatHome.name,
+  'accounts.ui'
 ]);
--- tchat/imports/components/tchatHome/tchatHome.js
+++ tchat/imports/components/tchatHome/tchatHome.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
 import { Tchats } from '../../api/tchats.js';
 
 import template from './tchatHome.html';
@@ -10,9 +11,29 @@ class TchatCtrl {
 
     this.helpers({
       tchats() {
-        return Tchats.find({});
+        return Tchats.find({}, {
+          sort: {
+            createdAt: +1
+          }
+        });
+      },
+      currentUser() {
+        return Meteor.user();
       }
     })
+  }
+
+  addMessage(newMessage) {
+    // Insert un message dans la bd
+    Tchats.insert({
+      text: newMessage,
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+
+    // Vider la zone de saisie
+    this.newMessage = '';
   }
 }
 
